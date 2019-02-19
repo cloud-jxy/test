2f125624d7a11771498dbc0983605049151dc0a0
jiangxueyun <xueyun0430@icloud.com>
2019-02-19 16:27:03 +0800

    车辆信息界面:绑定控件变量

--- TabCarDialog.cpp
+++ TabCarDialog.cpp
@@ -13,6 +13,13 @@ IMPLEMENT_DYNAMIC(TabCarDialog, CDialogEx)
 
 TabCarDialog::TabCarDialog(CWnd* pParent /*=NULL*/)
 	: CDialogEx(IDD_TAB_CAR_DIALOG, pParent)
+	, m_str_speed(_T(""))
+	, m_str_mil(_T(""))
+	, m_str_v(_T(""))
+	, m_str_i(_T(""))
+	, m_str_journey_speedup(_T(""))
+	, m_str_journy_barke(_T(""))
+	, m_str_dcdc_status(_T(""))
 {
 
 }
@@ -24,6 +31,21 @@ TabCarDialog::~TabCarDialog()
 void TabCarDialog::DoDataExchange(CDataExchange* pDX)
 {
 	CDialogEx::DoDataExchange(pDX);
+	DDX_Text(pDX, IDC_EDIT_CAR_STATUS, m_str_car_status);
+	DDX_Text(pDX, IDC_EDIT_CHARGING_STATUS, m_str_charging_status);
+	DDX_Text(pDX, IDC_EDIT_DRIVER_STATUS, m_str_driver_status);
+	DDX_Text(pDX, IDC_EDIT_BRAKING_STATUS, m_str_braking_status);
+	DDX_Text(pDX, IDC_EDIT_RUN_MODE, m_str_run_mode);
+	DDX_Text(pDX, IDC_EDIT_SPEED, m_str_speed);
+	DDX_Text(pDX, IDC_EDIT_GEARS, m_str_gears);
+	DDX_Text(pDX, IDC_EDIT_SOC, m_str_soc);
+	DDX_Text(pDX, IDC_EDIT_RESISTANCE, m_str_r);
+	DDX_Text(pDX, IDC_EDIT_MIL, m_str_mil);
+	DDX_Text(pDX, IDC_EDIT1_TOTAL_V, m_str_v);
+	DDX_Text(pDX, IDC_EDIT1_TOTAL_I, m_str_i);
+	DDX_Text(pDX, IDC_EDIT_JOURNEY_SPEEDUP, m_str_journey_speedup);
+	DDX_Text(pDX, IDC_EDIT1_JOURNEY_BRAKE, m_str_journy_barke);
+	DDX_Text(pDX, IDC_EDIT1_DCDC_STATUS, m_str_dcdc_status);
 }
 
 
--- TabCarDialog.h
+++ TabCarDialog.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "afxwin.h"
 
 
 // TabCarDialog 对话框
@@ -20,4 +21,20 @@ protected:
 	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 支持
 
 	DECLARE_MESSAGE_MAP()
+public:
+	CString m_str_car_status;
+	CString m_str_charging_status;
+	CString m_str_driver_status;
+	CString m_str_braking_status;
+	CString m_str_run_mode;
+	CString m_str_speed;
+	CString m_str_gears;
+	CString m_str_soc;
+	CString m_str_r;
+	CString m_str_mil;
+	CString m_str_v;
+	CString m_str_i;
+	CString m_str_journey_speedup;
+	CString m_str_journy_barke;
+	CString m_str_dcdc_status;
 };
